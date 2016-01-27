4f6f52cb8e0536882db9b6213698e098902cc4e9
Charles Carter and Michael Lor <michael.vang.1985@gmail.com>
2016-01-27 13:22:25 -0800

    implement prototypes in place of functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,29 @@
+var Word;
+
 function Word(wordInput) {
   this.wordInput = wordInput;
   };
 
-function generateWord() {
-  var randomWords = ["Pencil","Pen","Paper"];
-  var randomWord = randomWords[Math.floor(Math.random() * randomWords.length)];
-  // console.log(randomWords);
-  // console.log(randomWord);
-  return randomWord;
-}
+Word.prototype.pickRandomWord() {
+  var words = ["Pencil","Orange","Porcupine","Mailman"];
+  return words[Math.floor(Math.random() * words.length)];
+  };
 
-function splitWord(generateWord) {
-  var splitWord = this.generateWord();
-  console.log(splitWord);
-  return splitWord.split('');
-  // console.log(splitWord);
+//
+// function generateWord() {
+//   var randomWords = ["Pencil","Pen","Paper"];
+//   var randomWord = randomWords[Math.floor(Math.random() * randomWords.length)];
+//   return randomWord;
+// };
+//
+// function splitWord(generateWord) {
+//   var splitWord = this.generateWord();
+//   console.log(splitWord);
+//   return splitWord.split('');
+//   // console.log(splitWord);
 
-
-}
+//
+// };
 
 // $(document).ready(function() {
 //
--- spec/specs.js
+++ spec/specs.js
@@ -1,12 +1,25 @@
 describe('Word', function() {
-  it("will provide a word for the game.", function() {
-    var testWord = new Word ("Pencil");
-    expect(testWord.wordInput).to.equal("Pencil");
+  it("will provide functionality for the puzzle word.", function() {
+    var testWord = new Word ("input");
+    expect(testWord.wordInput).to.equal("input");
   });
-  it("will return a random word from the array of words.", function() {
-    expect(generateWord("Pencil")).to.equal("Pencil");
-  });
-  it("will split the selected random word into an array of separate letters.", function() {
-    expect(splitWord(generateWord)).to.eql(["P","e","n","c","i","l"]);
-  });
+  it("will return a random word from an array.", function() {
+    var testWord = new Word ();
+    expect(testWord.pickRandomWord).to.equal("Pencil");
+  })
 });
+
+
+// describe('Word', function() {
+//   it("will provide a word for the game.", function() {
+//     var testWord = new Word ("Pencil");
+//     expect(testWord.wordInput).to.equal("Pencil");
+//   });
+//   it("will return a random word from the array of words.", function() {
+//     var testWord = new Word ("");
+//     expect(testWord.generateWord).to.equal("Pencil");
+//   });
+//   it("will split the selected random word into an array of separate letters.", function() {
+//     expect(splitWord(generateWord)).to.eql(["P","e","n","c","i","l"]);
+//   });
+// });
