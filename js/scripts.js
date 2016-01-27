fc0ed9b6fbf8bbe7c63b4549fec78ba6e4a2a197
Charles Carter and Michael Lor <michael.vang.1985@gmail.com>
2016-01-27 09:59:11 -0800

    implement test for random word array

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,13 @@ function Word(wordInput) {
   this.wordInput = wordInput;
   };
 
+function selectWord(randomWord) {
+  var randomWords = ["Pencil","Forest","Skyline","Neptune"];
+  var randomWord = randomWords[Math.floor(Math.random()* randomWords.length)];
+  console.log(randomWords);
+  console.log(randomWord);
+  return randomWord;
+}
 
 // $(document).ready(function() {
 //
--- spec/specs.js
+++ spec/specs.js
@@ -4,3 +4,13 @@ describe('Word', function() {
     expect(testWord.wordInput).to.equal("Pencil");
   });
 });
+
+describe('selectWord', function() {
+  it("will return a random word from the array of words.", function() {
+    var testselectWord = new selectWord ("Pencil");
+    expect(testselectWord.randomWord).to.equal("Pencil");
+  });
+  it("will separate the letters of the selected random word.", function() {
+  
+  })
+});
